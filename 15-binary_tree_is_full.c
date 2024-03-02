1af0b722dd1b12ffdf0a92f0f7b0d4bd94ea3a8f
Adham <adhamelsa8er@gmail.com>
2024-03-01 17:49:57 -0800

    a

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,11 +11,13 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if ((tree->left != NULL && tree->right == NULL) ||
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
 		    (tree->left == NULL && tree->right != NULL) ||
 		    binary_tree_is_full(tree->left) == 0 ||
 		    binary_tree_is_full(tree->right) == 0)
 			return (0);
-
+	}
 	return (1);
 }
